b1cefed9bf54f54db664e9fdc78f3ca1808b99a3
Lecks <leck.she@gmail.com>
2014-06-15 13:18:13 +1000

    admin theme

--- themes/dark/blank.php
+++ themes/dark/blank.php
@@ -1,2 +1,6 @@
+<div class="panel panel-default">
+<div class="panel panel-body">
 <h1>Looks like you don't have any images yet</h1>
 <h1><small>Log in to your <a href='<?php echo $config['path']?>admin'>Control Panel</a> to add some.</small></h1>
+</div>
+</div>
